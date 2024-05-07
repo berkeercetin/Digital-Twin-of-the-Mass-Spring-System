baae030d84ac25b82b12c8bba6dd03875cdc48d6
Berke Erçetin <berkeercetin@gmail.com>
2024-05-08 00:24:48 +0300

    dev

--- pytorch_PINN.py
+++ pytorch_PINN.py
@@ -10,16 +10,29 @@ else:
 
 device = torch.device(dev)
 
-k,m = 20.45, 0.24
-mu=m
-x0 = 0.08  # m 0.1
-v0 = 0  # m/s 0
+d, w0 = 2, 20
+mu, k = 2*d, w0**2
 
-def exact_solution(t):
-    w=np.sqrt(k/m)
-    u=x0*np.cos(w*t)
+# k,m = 20.45, 0.24
+# mu=m
+# x0 = 0.08  # m 0.1
+# v0 = 0  # m/s 0
+
+# def exact_solution(t):
+#     w=np.sqrt(k/m)
+#     u=x0*np.cos(w*t)
+#     return u
+
+def exact_solution(d, w0, t):
+    "Defines the analytical solution to the under-damped harmonic oscillator problem above."
+    assert d < w0
+    w = np.sqrt(w0**2-d**2)
+    phi = np.arctan(-d/w)
+    A = 1/(2*np.cos(phi))
+    cos = torch.cos(phi+w*t)
+    exp = torch.exp(-d*t)
+    u = exp*2*A*cos
     return u
-
 
 """
 def exact_solution(d, w0, t):
@@ -71,7 +84,7 @@ def main(sensor_x_data):
     t_physics = torch.linspace(0,len(sensor_x_data),30).view(-1,1).requires_grad_(True)
 
     t_test = torch.linspace(0,len(sensor_x_data),300).view(-1,1)
-    u_exact = exact_solution(t_test)
+    u_exact = exact_solution(d, w0, t_test)
 
     optimiser = torch.optim.Adam(pinn.parameters(),lr=1e-3)
     
@@ -84,17 +97,37 @@ def main(sensor_x_data):
         # using the following hyperparameters:
         lambda1, lambda2, lambda3 = 1e-1, 1e-4, 1e-2  # lambda3, sensör verileri için eklenen yeni bir hiperparametre
 
-        # compute boundary loss
-        u = pinn(t_boundary)
-        loss1 = (torch.squeeze(u) - x0)**2
-        dudt = torch.autograd.grad(u, t_boundary, torch.ones_like(u), create_graph=True)[0]
-        loss2 = (torch.squeeze(dudt) - v0)**2
 
+            # compute boundary loss
+        # TODO: write code here
+        u = pinn(t_boundary)
+        loss1 = (torch.squeeze(u) - 1)**2
+        dudt = torch.autograd.grad(u, t_boundary, torch.ones_like(u), create_graph=True)[0]
+        loss2 = (torch.squeeze(dudt) - 0)**2
+        
         # compute physics loss
+        # TODO: write code here
         u = pinn(t_physics)
         dudt = torch.autograd.grad(u, t_physics, torch.ones_like(u), create_graph=True)[0]
         d2udt2 = torch.autograd.grad(dudt, t_physics, torch.ones_like(dudt), create_graph=True)[0]
-        loss3 = torch.mean((mu*d2udt2 + k*u)**2)
+        loss3 = torch.mean((d2udt2 + mu*dudt + k*u)**2)
+
+
+
+
+
+
+        # compute boundary loss
+        # u = pinn(t_boundary)
+        # loss1 = (torch.squeeze(u) - x0)**2
+        # dudt = torch.autograd.grad(u, t_boundary, torch.ones_like(u), create_graph=True)[0]
+        # loss2 = (torch.squeeze(dudt) - v0)**2
+
+        # # compute physics loss
+        # u = pinn(t_physics)
+        # dudt = torch.autograd.grad(u, t_physics, torch.ones_like(u), create_graph=True)[0]
+        # d2udt2 = torch.autograd.grad(dudt, t_physics, torch.ones_like(dudt), create_graph=True)[0]
+        # loss3 = torch.mean((mu*d2udt2 + k*u)**2)
 
 
 
@@ -102,12 +135,12 @@ def main(sensor_x_data):
         #sensör verileri için bir kayıp hesaplanıyor ve ardından bu kayıp, modelin sensör verilerini doğru bir şekilde öğrenip öğrenmediğini belirlemek için kullanılıyor.
         # Kayıp hesaplanırken, tahmin edilen sensör verileri (u) ile gerçek sensör verileri (sensor_data) arasındaki ortalama kare hatası hesaplanıyor.
         # Eğer kayıp düşükse, bu, modelin sensör verilerini doğru bir şekilde öğrendiğini gösterebilir.
-        u = pinn(sensor_data)
-        loss_sensor = torch.mean((u - sensor_data)**2)  # MSE kullanılarak hesaplama
+        #u = pinn(sensor_data)
+        #loss_sensor = torch.mean((u - sensor_data)**2)  # MSE kullanılarak hesaplama
 
         # backpropagate joint loss, take optimiser step
-        loss = loss1 + lambda1*loss2 + lambda2*loss3 + lambda3*loss_sensor
-        #loss = loss1 + lambda1*loss2 + lambda2*loss3
+        #loss = loss1 + lambda1*loss2 + lambda2*loss3 + lambda3*loss_sensor
+        loss = loss1 + lambda1*loss2 + lambda2*loss3
         loss.backward()
         optimiser.step()
 
@@ -137,4 +170,4 @@ def main(sensor_x_data):
 #07.05.2924
 # Egitim zamani egitim datasinda nasil degisecek ?
 
-# yay durmuyor bizim modelimizde.
+# t = x ani icin f(t) = y ani nasil ayarlanacak ?
