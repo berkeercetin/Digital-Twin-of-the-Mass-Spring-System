bd4c6794663c66d29e3a4ed40f5a768677749ea5
Berke Erçetin <berkeercetin@gmail.com>
2024-05-09 15:34:36 +0300

    fix bugs

--- server.py
+++ server.py
@@ -1,33 +1,36 @@
+import time
 import socket
 import pytorch_PINN
-
-
+import pickle
 
 HOST = "192.168.137.1"  # Standard loopback interface address (localhost)
 PORT = 12000  # Port to listen on (non-privileged ports are > 1023)
+datam = []
 
-dataArray = []
-# Soket işlemleri
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
     conn, addr = s.accept()
     with conn:
         print(f"Connected by {addr}")
-
         while True:
-            data = conn.recv(1024)
+            received_data = conn.recv(2048).decode('utf-8')
+            print(received_data)
+            data = received_data.split('0.')
+            data = [x for x in data if x != '']
+            for i in range(len(data)):
+                if data[i] != '':
+                    data[i] = '0.'+data[i]
+                    data[i] = float(data[i]) 
+            print(data)
+            #datam.extend([float(x) for x in received_data.split("0.") if x != ""])  # "0." ile başlayan her öğeyi ayır            print(datam)
+            #time.sleep(10)
+
+            pytorch_PINN.main(data)
+            
+            #pytorch_PINN.main(pickle.loads(data))
             if not data:
                 break
-            data = data.decode('utf-8')
-            data_chunks = data.split("0.")  # Veriyi "0." kısmından böler
-            for chunk in data_chunks:
-                if chunk != "":
-                    number = float("0." + chunk)
-                    dataArray.append(number)
-
-            print(dataArray)
-            pytorch_PINN.main(dataArray)
 
             
 
